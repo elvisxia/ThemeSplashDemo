52667381d9ccf66b920ead0fe61944fa3a19c585
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <xux@microsoft.com>
2018-03-05 11:34:25 +0800

    Add project files.

---
+++ ThemeSplashDemo/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace ThemeSplashDemo
+{
+    [Activity(Label = "ThemeSplashDemo")]
+    public class MainActivity : Activity
+    {
+        Button btnSave;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            btnSave = FindViewById<Button>(Resource.Id.btnSave);
+            btnSave.Click += BtnSave_Click;
+        }
+
+        private void BtnSave_Click(object sender, System.EventArgs e)
+        {
+            ISharedPreferences preference=GetSharedPreferences("SplashTheme", Android.Content.FileCreationMode.Private);
+            ISharedPreferencesEditor editor= preference.Edit();
+            editor.PutString("themeName", "AppTheme2");
+            editor.Commit();
+        }
+    }
+}
+
---
+++ ThemeSplashDemo/SplashActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace ThemeSplashDemo
+{
+    [Activity(Label = "SplashActivity",MainLauncher =true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            ISharedPreferences preferences=GetSharedPreferences("SplashThemeId", FileCreationMode.Private);
+            var themeName=preferences.GetString("themeName","AppTheme");
+            int themeId = Resource.Style.AppTheme ;
+            switch (themeName)
+            {
+                case "AppTheme":
+                    themeId = Resource.Style.AppTheme;
+                    break;
+                case "AppTheme2":
+                    themeId = Resource.Style.AppTheme2;
+                    break;
+            }
+            SetTheme(themeId);
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Splash);
+
+            ScheduleSplashScreen();
+            
+            // Create your application here
+        }
+
+        private void ScheduleSplashScreen()
+        {
+            var splashScreenDuration = 2000;
+            Handler handler = new Handler();
+            handler.PostDelayed(() =>
+            {
+                RouteToAppropriatePage();
+                Finish();
+            }, splashScreenDuration);
+
+        }
+
+        public void RouteToAppropriatePage()
+        {
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+        }
+    }
+}
